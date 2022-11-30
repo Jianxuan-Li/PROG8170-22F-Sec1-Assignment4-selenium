f478dfc475bd19b2c5c28c3e599d9e40cb79549d
Jack Li <49817817+Jianxuan-Li@users.noreply.github.com>
2022-11-30 15:43:53 -0500

    Add project files.

---
+++ Assignment4/Assignment4Test.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+
+namespace Assignment4
+{
+    [TestFixture]
+    public class Assignment4Test
+    {
+        private IWebDriver driver;
+        public IDictionary<string, object> vars { get; private set; }
+        private IJavaScriptExecutor js;
+        [SetUp]
+        public void SetUp()
+        {
+            driver = new ChromeDriver();
+            js = (IJavaScriptExecutor)driver;
+            vars = new Dictionary<string, object>();
+        }
+        [TearDown]
+        protected void TearDown()
+        {
+            driver.Quit();
+        }
+
+        [Test]
+        public void age25Experience3Accident0()
+        {
+            driver.Navigate().GoToUrl("http://localhost/getQuote.html");
+            driver.Manage().Window.Size = new System.Drawing.Size(1550, 830);
+            driver.FindElement(By.Id("firstName")).Click();
+            driver.FindElement(By.Id("firstName")).SendKeys("Jianxuan");
+            driver.FindElement(By.Id("lastName")).SendKeys("Li");
+            driver.FindElement(By.Id("address")).SendKeys("111 University Ave");
+            driver.FindElement(By.Id("city")).Click();
+            driver.FindElement(By.Id("city")).SendKeys("Waterloo");
+            driver.FindElement(By.Id("postalCode")).Click();
+            driver.FindElement(By.Id("postalCode")).SendKeys("N2K 0H2");
+            driver.FindElement(By.Id("phone")).Click();
+            driver.FindElement(By.Id("phone")).SendKeys("111-111-1111");
+            driver.FindElement(By.Id("email")).Click();
+            driver.FindElement(By.Id("email")).SendKeys("liujin834@gmail.com");
+            driver.FindElement(By.CssSelector("body")).Click();
+            driver.FindElement(By.Id("age")).Click();
+            driver.FindElement(By.Id("age")).SendKeys("25");
+            driver.FindElement(By.Id("experience")).Click();
+            driver.FindElement(By.Id("experience")).SendKeys("3");
+            driver.FindElement(By.Id("accidents")).Click();
+            driver.FindElement(By.Id("accidents")).SendKeys("0");
+            driver.FindElement(By.Id("btnSubmit")).Click();
+            {
+                string value = driver.FindElement(By.Id("finalQuote")).GetAttribute("value");
+                Assert.That(value, Is.EqualTo("$2500"));
+            }
+        }
+    }
+}
